71965338ae0203b178e9f70744afcba7a4fdde82
shulbaeva <j.shulbaeva@cft.ru>
2021-06-07 17:24:03 +0700

    add doc tests

--- src/test/java/tests/DocFile.java
+++ src/test/java/tests/DocFile.java
@@ -1,4 +1,21 @@
 package tests;
 
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static tweaks.Files.readTextFromPath;
+
 public class DocFile {
+    @Test
+    void checkDocFile() throws IOException {
+        String Path = "./src/test/resources/files/doc.doc";
+        String expectedData = "I FEEL SUCCESS";
+        String actualData = readTextFromPath(Path);
+
+        assertThat(actualData, containsString(expectedData));
+
+    }
 }
